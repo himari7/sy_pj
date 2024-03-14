3b440e3eb9603d97430c57e745207862eedd92a8
changhyun9 <ckdgus1713@naver.com>
2024-03-15 00:42:26 +0900

    init commit

---
+++ src/main/java/com/ll/maybeMap/MaybeMapApplication.java
@@ -0,0 +1,13 @@
+package com.ll.maybeMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaybeMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaybeMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/maybeMap/MaybeMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.maybeMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaybeMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
